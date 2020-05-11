a81d3b6f4975ecf5971609b97486ebd927a6a021
mehtapbasaran <mehtap_gs_95@hotmail.com>
2020-05-11 14:51:13 +0300

    Add project files.

---
+++ SpringBlog/Scripts/Site.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+
+    bsCustomFileInput.init();
+
+    $("#frmSearch").submit(function (event) {
+
+        var q = $("#q").val().trim();
+
+        $("#q").val(q);
+
+        if (!q) {
+            event.preventDefault();
+        }
+
+    });
+
+});
---
+++ SpringBlog/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using X.PagedList;
+using SpringBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpringBlog.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IPagedList<Post> Posts { get; set; }
+
+        public Category Category { get; set; }
+
+        public string SearchTerm { get; set; }
+
+        public int? CategoryId { get; set; }
+    }
+}
