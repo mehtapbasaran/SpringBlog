5ef0af248ddcf7d81ea00ff3fe03c6fc9f19d7c8
mehtapbasaran <mehtap_gs_95@hotmail.com>
2020-05-12 19:41:07 +0300

    comment approve

--- SpringBlog/Areas/Admin/dist/js/admin.custom.js
+++ SpringBlog/Areas/Admin/dist/js/admin.custom.js
@@ -1,20 +1,29 @@
 ﻿$(function () {
     bsCustomFileInput.init();
 
-    $('table[data-table="true"]').DataTable({
-        "responsive": true,
-        "autoWidth": false,
+    $('table[data-table="true"]').each(function (index) {
+
+        var sortOrder = $(this).data("table-sort-order") || "asc";
+        var sortColumn = $(this).data("table-sort-column") || 0;
+
+        $(this).DataTable({
+            "order": [[sortColumn, sortOrder]],
+            "responsive": true,
+            "autoWidth": false,
+        });
+
     });
+
+
 
     $('textarea[data-snote="true"]').summernote({
         height: 200
     });
 
-
     // https://getbootstrap.com/docs/4.4/components/modal/#via-javascript
     $("body").on("click", "[data-delete-id]", function (event) {
         event.preventDefault();
-        var button = $(this); // Button that triggered the modal
+        var button = $(this); // Button that triggered the modal  event-target
         var id = button.data('delete-id') // Extract info from data-* attributes
         var name = button.data('delete-name') // Extract info from data-* attributes
         var action = button.data('delete-action') // Extract info from data-* attributes
@@ -23,4 +32,6 @@
         $("#deleteModalItemId").val(id);
         $("#deleteModal").modal();
     });
+
+
 });
